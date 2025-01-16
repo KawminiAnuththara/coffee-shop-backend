d135514bc2272f4e582cf321148c3c7e1a293ca2
kawmini <anuththarakawmini@gmail.com>
2025-01-16 20:06:44 +0530

    add properties😉

--- src/main/java/com/coffee/CoffeeShopApplication.java
+++ src/main/java/com/coffee/CoffeeShopApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoffeeShopApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CoffeeShopApplication.class, args);
 	}
 
